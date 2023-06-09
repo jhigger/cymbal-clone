e5b0169329ef7fd0f60b3f24124173206229d954
jhigger <njohnjhigger@gmail.com>
2023-06-09 18:39:28 +0800

    fix responsiveness

--- src/components/Cards.tsx
+++ src/components/Cards.tsx
@@ -4,7 +4,7 @@ const Cards = () => {
 			title: "Step 1",
 			desc: "Add Cymbal Bot to your server by logging in with your Discord account.",
 			figure: (
-				<div className="flex items-center justify-center bg-indigo-500">
+				<div className="flex h-full w-full items-center justify-center bg-indigo-500">
 					<a
 						className="btn rounded-full border-0 bg-white px-8 normal-case text-indigo-500 hover:bg-indigo-600 hover:text-white"
 						href="#"
@@ -18,12 +18,12 @@ const Cards = () => {
 			title: "Step 2",
 			desc: "Choose a collection to track! You can pick any major collection from both Ethereum and Solana.",
 			right: false,
-			figure: <div className="bg-blue-500" />,
+			figure: <div className="h-full w-full bg-blue-500" />,
 		},
 		{
 			title: "Step 3",
 			desc: "Get real-time NFT sales data piped straight into your Discord server — all for free.",
-			figure: <div className="bg-yellow-500" />,
+			figure: <div className="h-full w-full bg-yellow-500" />,
 		},
 	];
 
@@ -46,18 +46,18 @@ type CardType = {
 const Card = (props: CardType) => {
 	const { title, desc, figure, right = true } = props;
 
+	const Figure = () => {
+		return <figure className="h-40 lg:w-96">{figure}</figure>;
+	};
+
 	return (
 		<div className="card bg-base-100 shadow-2xl lg:card-side lg:max-w-[733px]">
-			{!right && (
-				<figure className="h-40 w-80 overflow-visible">{figure}</figure>
-			)}
-			<div className="card-body">
+			{!right && <Figure />}
+			<div className="card-body w-full">
 				<h2 className="card-title">{title}</h2>
-				<p className="w-96">{desc}</p>
+				<p>{desc}</p>
 			</div>
-			{right && (
-				<figure className="h-40 w-80 overflow-visible">{figure}</figure>
-			)}
+			{right && <Figure />}
 		</div>
 	);
 };
