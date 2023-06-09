b9697e7430beaf1da9acc1e1861fa6f5a634cc41
jhigger <njohnjhigger@gmail.com>
2023-06-09 17:41:09 +0800

    hero and card

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,6 +3,34 @@ import { type NextPage } from "next";
 import Head from "next/head";
 
 const Home: NextPage = () => {
+	const steps: CardType[] = [
+		{
+			title: "Step 1",
+			desc: "Add Cymbal Bot to your server by logging in with your Discord account.",
+			figure: (
+				<div className="flex items-center justify-center bg-indigo-500">
+					<a
+						className="btn rounded-full border-0 bg-white px-8 normal-case text-indigo-500 hover:bg-indigo-600 hover:text-white"
+						href="#"
+					>
+						Sign in with Discord
+					</a>
+				</div>
+			),
+		},
+		{
+			title: "Step 2",
+			desc: "Choose a collection to track! You can pick any major collection from both Ethereum and Solana.",
+			right: false,
+			figure: <div className="bg-blue-500" />,
+		},
+		{
+			title: "Step 3",
+			desc: "Get real-time NFT sales data piped straight into your Discord server — all for free.",
+			figure: <div className="bg-yellow-500" />,
+		},
+	];
+
 	return (
 		<>
 			<Head>
@@ -13,24 +41,10 @@ const Home: NextPage = () => {
 			<main className="min-h-screen">
 				<div className="container mx-auto flex flex-col items-center justify-center p-4">
 					<Hero />
-					<div className="card bg-base-100 shadow-2xl lg:card-side lg:max-w-[733px]">
-						<div className="card-body">
-							<h2 className="card-title">Step 1</h2>
-							<p>
-								Add Cymbal Bot to your server by logging in with
-								your Discord account.
-							</p>
-						</div>
-						<figure className="overflow-visible bg-indigo-500">
-							<div className="flex h-40 w-80 items-center justify-center">
-								<a
-									className="btn rounded-full border-0 bg-white px-8 normal-case text-indigo-500 hover:bg-indigo-600 hover:text-white"
-									href="#"
-								>
-									Sign in with Discord
-								</a>
-							</div>
-						</figure>
+					<div className="grid gap-14">
+						{steps.map((step) => {
+							return <Card key={step.title} {...step} />;
+						})}
 					</div>
 				</div>
 			</main>
@@ -38,19 +52,50 @@ const Home: NextPage = () => {
 	);
 };
 
+type CardType = {
+	title: string;
+	desc: string | JSX.Element;
+	figure?: JSX.Element;
+	right?: boolean;
+};
+
+const Card = (props: CardType) => {
+	const { title, desc, figure, right = true } = props;
+
+	return (
+		<div className="card bg-base-100 shadow-2xl lg:card-side lg:max-w-[733px]">
+			{!right && (
+				<figure className="h-40 w-80 overflow-visible">{figure}</figure>
+			)}
+			<div className="card-body">
+				<h2 className="card-title">{title}</h2>
+				<p className="w-96">{desc}</p>
+			</div>
+			{right && (
+				<figure className="h-40 w-80 overflow-visible">{figure}</figure>
+			)}
+		</div>
+	);
+};
+
 const Hero = () => {
 	return (
 		<div className="hero py-40">
 			<div className="hero-content text-center">
-				<div className="max-w-md">
-					<h1 className="text-5xl font-bold">
-						Supercharge your Discord server
-					</h1>
-					<p className="py-6">
-						Real-time NFT sales in your Discord server — all for
-						free.
-					</p>
-					<button className="btn-accent btn-outline btn rounded-full">
+				<div className="flex max-w-md flex-col items-center justify-center gap-4">
+					<div className="mx-auto w-max rounded-lg bg-slate-700 px-2 py-1.5 text-sm font-bold text-white">
+						FREE NFT SALES BOT
+					</div>
+					<div>
+						<h1 className="text-5xl font-bold lg:w-max">
+							Supercharge your Discord server
+						</h1>
+						<p className="py-6">
+							Real-time NFT sales in your Discord server — all for
+							free.
+						</p>
+					</div>
+					<button className="btn-accent btn-outline btn rounded-full px-8 shadow-2xl">
 						Setup Cymbal Bot
 					</button>
 				</div>
